e52b2cfd8ad5904393ad1c98f4b6cc33d4a58d11
Sergio Silva <sergiobam159@gmail.com>
2025-09-14 17:46:44 -0500

    configuración de entidad para autoseteo de id en mongo

---
+++ src/main/java/com/clientes/api_clientes/constantes/TipoDispositivo.java
@@ -0,0 +1,6 @@
+package com.clientes.api_clientes.constantes;
+
+public enum TipoDispositivo {
+    IOS,
+    AND
+}
--- src/main/java/com/clientes/api_clientes/controlador/ClienteControlador.java
+++ src/main/java/com/clientes/api_clientes/controlador/ClienteControlador.java
@@ -1,10 +1,13 @@
 package com.clientes.api_clientes.controlador;
 
+import com.clientes.api_clientes.constantes.TipoDispositivo;
 import com.clientes.api_clientes.dto.ClienteListadoDTO;
 import com.clientes.api_clientes.dto.ClienteRequestDTO;
 import com.clientes.api_clientes.dto.ClienteResponseDTO;
 import com.clientes.api_clientes.servicio.ClienteServicio;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,13 +20,46 @@ public class ClienteControlador {
     private ClienteServicio clienteServicio;
 
     @PostMapping
-    public ClienteResponseDTO crearCliente (@Valid @RequestBody ClienteRequestDTO cliente){
-        return clienteServicio.crearCliente(cliente);
+    public ClienteResponseDTO crearCliente (
+            @RequestHeader("consumerId")
+            @NotBlank(message = "consumerId es obligatorio")
+            String consumerId,
+            @RequestHeader("traceparent")
+            @Pattern(
+                    regexp = "^[0-9a-f]{2}-[0-9a-f]{32}-[0-9a-f]{16}-[0-9a-f]{2}$",
+                    flags = Pattern.Flag.CASE_INSENSITIVE,
+                    message = "error en el formato del traceparent, debe seguir el formato de W3C"
+            )String traceparent,
+            @RequestHeader("deviceType")
+            @NotBlank(message = "tipoDispositivo es obligatorio")
+            TipoDispositivo tipoDispositivo,
+            @RequestHeader("deviceId")
+            @NotBlank(message = "deviceId es obligatorio")
+            String deviceId,
+            @Valid
+            @RequestBody ClienteRequestDTO cliente){
+
+            return clienteServicio.crearCliente(cliente);
     }
 
     @GetMapping
-    public List<ClienteListadoDTO> listarclientes(){
-            return clienteServicio.listarClientes();
+    public List<ClienteListadoDTO> listarclientes(
+            @RequestHeader("consumerId") @NotBlank String consumerId,
+            @RequestHeader("traceparent")
+            @NotBlank
+            @Pattern(
+                    regexp = "^[0-9a-f]{2}-[0-9a-f]{32}-[0-9a-f]{16}-[0-9a-f]{2}$",
+                    flags = Pattern.Flag.CASE_INSENSITIVE
+            )
+            String traceparent,
+            @RequestHeader("deviceType") TipoDispositivo tipoDispositivo,
+            @RequestHeader("deviceId") @NotBlank String deviceId
+    ){
+
+        return clienteServicio.listarClientes();
+
     }
+
+
 
 }
